fb35cc27ded2b773c4093ca85dcf9be08a1b7d8a
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2025-11-07 10:45:03 +0530

    I made some Improvements and tested

--- dinamic_connectivity.c
+++ dinamic_connectivity.c
@@ -1,59 +1,84 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
 // prototypes
-void initlize_sequence(int arr[], int size);
-int quick_Find(int arr[],int p,int q,int size);
-void quick_Find_Union(int arr[],int p,int q,int size); 
+void initialize_sequence(int arr[], int size);
+int quick_Find(int arr[], int p, int q, int size);
+void quick_Find_Union(int arr[], int p, int q, int size);
 void shuffle_Static_Array(int arr[], int size);
 void swap_Integers(int *a, int *b);
 
-int main(void){
-  // It won't be random for every exicuation without seeding
+int main(void) {
+  // It won't be random for every execution without seeding
   srand(time(NULL));
 
   int size = 0;
   printf("How many numbers of elements do you need: ");
   scanf("%d", &size);
-  
+  if (size <= 0) {
+    printf("Invalid size length. It should be greater then 0.");
+    return 1;
+  }
+
   int connection_count = 0;
   printf("\nconnection counts: ");
   scanf("%d", &connection_count);
-  
+  if (connection_count <= 0) {
+    printf("Invalid Connection counts. It should be greater then 0.");
+    return 2;
+  }
   int arr[size];
-  initlize_sequence(arr, size);
+  initialize_sequence(arr, size);
 
-  
   printf("\nUnion Format <number> connect to <number>: \n");
-  for(int i = 0; i < connection_count; i++) {
+  for (int i = 0; i < connection_count; i++) {
     int p, q;
     scanf("%d %d", &p, &q);
-    if ((p >= size) && (q >= size)) {
-      printf("\n Invalid randge, Provide agian!\n");
-      connection_count++;
+    if (p < 0 || q < 0 || p >= size || q >= size) {
+      printf("\n Invalid range, Provide again!\n");
+      i--;
+      continue;
     }
     quick_Find_Union(arr, p, q, size);
   }
-  
-  printf("\nResult: ");
-  for(int i = 0; i < size; i++) {
-    printf("%d ", arr[i]);
+
+  int choise;
+  while (1) {
+
+    printf("Do you want to check connection type 1 for yes anything else for "
+           "not: ");
+    choise = 0;
+    scanf("%d", &choise);
+    if (choise != 1) {
+      break;
+    }
+
+    printf("Connection check Request Format <number> to <number>: \n");
+    int p = -1;
+    int q = -1;
+    scanf("%d %d", &p, &q);
+    if ((p >= size) || q >= size || p < 0 || q < 0) {
+      printf(
+          "\n Invalid Quick Find request for connection %d %d within range %d",
+          p, q, size);
+      continue;
+    }
+
+    if (quick_Find(arr, p, q, size)) {
+      printf("\nConnected!\n");
+    } else {
+      printf("\n Not Connected!\n");
+    }
   }
-  printf("\n");
-  
   return 0;
 }
 
-
-
-
-
-// it generates a random list that can be used as joining requests because all elements are distinct
+// it generates a random list that can be used as joining requests because all
+// elements are distinct
 void shuffle_Static_Array(int arr[], int size) {
 
-    for (int i = size - 1; i > 0; i--) {
+  for (int i = size - 1; i > 0; i--) {
     int j = rand() % (i + 1);
     swap_Integers(&arr[i], &arr[j]);
   }
@@ -65,18 +90,20 @@ void swap_Integers(int *a, int *b) {
   *b = temp;
 }
 
-void initlize_sequence(int arr[], int size) {
+void initialize_sequence(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     arr[i] = i;
   }
 }
 
 // arr[i] = j means i is connected to j which is also a index of arr
-void quick_Find_Union(int arr[],int p,int q,int size) {
-  if ( ((p >= size) & (q >= size)) || (size < 0) ){
-    printf("\n Invalid Quick Find request for connection %d %d within range %d", p, q, size);
-    exit(EXIT_FAILURE);
-  } 
+void quick_Find_Union(int arr[], int p, int q, int size) {
+  // if (((p >= size) && (q >= size)) || (size < 0)) { wrong
+  if ((p >= size) || q >= size || p < 0 || q < 0) {
+    printf("\n Invalid Quick Find request for connection %d %d within range %d",
+           p, q, size);
+    exit(3);
+  }
   int pid = arr[p];
   int qid = arr[q];
   for (int i = 0; i < size; i++) {
@@ -84,13 +111,14 @@ void quick_Find_Union(int arr[],int p,int q,int size) {
       arr[i] = qid;
     }
   }
-} 
-
-int quick_Find(int arr[],int p,int q,int size) {
-  if ( ((p >= size) && (q >= size)) || (size < 0) ){
-    printf("\n Invalid Quick Find request for connection %d %d within range %d", p, q, size);
-    exit(EXIT_FAILURE);
-  } 
-  return (arr[p] == arr[q]);
 }
 
+int quick_Find(int arr[], int p, int q, int size) {
+  //  if (((p >= size) && (q >= size)) || (size < 0)) {
+  if ((p >= size) || q >= size || p < 0 || q < 0) {
+    printf("\n Invalid Quick Find request for connection %d %d within range %d",
+           p, q, size);
+    exit(4);
+  }
+  return (arr[p] == arr[q]);
+}
